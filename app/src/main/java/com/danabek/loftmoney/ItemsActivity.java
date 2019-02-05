1d9b2e427b7bbbfb2359c6bf4f8c8f21eb37f6e4
Kartbayev Danabek <danabek55@gmail.com>
2019-02-06 00:21:31 +0600

    Added DividerItemDecoration

--- app/src/main/java/com/danabek/loftmoney/ItemsActivity.java
+++ app/src/main/java/com/danabek/loftmoney/ItemsActivity.java
@@ -2,6 +2,7 @@ package com.danabek.loftmoney;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -17,11 +18,14 @@ public class ItemsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
         adapter = new ItemsAdapter(this);
         recycler = findViewById(R.id.recycler);
         recycler.setAdapter(adapter);
         recycler.setLayoutManager(new LinearLayoutManager(this));
-
+        RecyclerView.LayoutManager layoutManager = (new LinearLayoutManager(this));
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recycler.getContext(),((LinearLayoutManager) layoutManager).getOrientation());
+        recycler.addItemDecoration(dividerItemDecoration);
 
         List<ItemPosition> items = new ArrayList<>();
         items.add(new ItemPosition("Milk","230"));
